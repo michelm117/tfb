89f95e37f2bb8cb67a45bcb98df9d4c3b9b1fa6e
Marc Michel <marc@michel.lu>
2022-08-26 21:55:55 +0200

    refactor(web): add heading component

---
+++ libs/web/shared/src/lib/heading/heading.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tfb-heading',
+  templateUrl: './heading.component.html',
+  styleUrls: ['./heading.component.scss'],
+})
+export class HeadingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- libs/web/shared/src/lib/web-shared.module.ts
+++ libs/web/shared/src/lib/web-shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SocialBarComponent } from './social-bar/social-bar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RiderCardComponent } from './rider-card/rider-card.component';
+import { HeadingComponent } from './heading/heading.component';
 
 @NgModule({
   imports: [CommonModule, FontAwesomeModule],
-  declarations: [SocialBarComponent, RiderCardComponent],
+  declarations: [SocialBarComponent, RiderCardComponent, HeadingComponent],
   exports: [SocialBarComponent, RiderCardComponent],
 })
 export class WebSharedModule {}
