dda7a0e6822ce4d21683c9da7452349c4aed0bdb
Marc Michel <marc@michel.lu>
2022-08-26 20:08:06 +0200

    refactor(web): add footer

--- libs/web/layout/src/lib/footer/footer.component.ts
+++ libs/web/layout/src/lib/footer/footer.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  faInstagram,
+  faStrava,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'tfb-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
-  constructor() {}
+export class FooterComponent {
+  faInstagram = faInstagram;
+  faStrava = faStrava;
+  faTwitter = faTwitter;
 
-  ngOnInit(): void {}
+  year = new Date().getFullYear();
+
+  scrollToTop() {
+    (function smoothscroll() {
+      const currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 550) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - currentScroll / 20);
+      }
+    })();
+  }
 }
